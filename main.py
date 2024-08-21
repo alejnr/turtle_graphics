1c0e6d6886c620e4d5ab67c616c2a9024cc38fba
Alex Menlah <alexamenlah@gmail.com>
2024-08-21 04:56:36 +0100

    final code: accessing attr and meths

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 slowy = Turtle()
 
+# print(slowy)
+slowy.shape("turtle")
+
+slowy.color("blue")
+
+# move the turtle forward by 100 paces
+slowy.forward(100)
+
+Screen().exitonclick()
+
+print(Screen().canvheight)
